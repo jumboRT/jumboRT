a9c0b6db1455e2ee2cdac067795d3149d15bfa45
Chen Steenvoorden <csteenvo@student.codam.nl>
2022-06-14 10:09:34 +0200

    remove benchmark files
    Former-commit-id: 49eec30d02a627331d23948287b6681808297677

--- src/math/compile.sh
+++
@@ -1,1 +0,0 @@
-clang++ benchmark.cc -I../../include -Ibenchmark/include -Ibenchmark/build/include -O3 -lbenchmark -Lbenchmark/build/src
--- src/math/float_cmp.c
+++ src/math/float_cmp.c
@@ -1,5 +1,6 @@
 #include "rtmath.h"
 
+/* https://bitbashing.io/comparing-floats.html */
 __attribute__ ((const))
 int
 	float_eq(FLOAT a, FLOAT b, FLOAT error)
