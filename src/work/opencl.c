15905049124dd9eba9d41d420f9ebe025cbab47c
Chen Steenvoorden <csteenvo@student.codam.nl>
2022-07-11 13:39:13 +0200

    async opencl
    Former-commit-id: 53a0f5c8968ea7cbe89888834904e3e5c83fed3c

--- src/work/opencl.c
+++ src/work/opencl.c
@@ -12,7 +12,7 @@
 # endif
 
 # ifndef RT_WORK_OPENCL_GLOBAL_SIZE
-#  define RT_WORK_OPENCL_GLOBAL_SIZE 1048576
+#  define RT_WORK_OPENCL_GLOBAL_SIZE (1ULL << 20)
 # endif
 
 # ifndef RT_WORK_OPENCL_LOCAL_SIZE
@@ -21,13 +21,13 @@
 
 struct s_opencl_ctx {
 	cl_context			context;
-	cl_command_queue	command_queue;
+	cl_command_queue	command_queue[2];
 	cl_program			program;
 	cl_kernel			work_kernel;
 	cl_kernel			set_ptr_kernel;
 	cl_mem				world_mem;
 	cl_mem				ctx_mem;
-	cl_mem				result_mem;
+	cl_mem				result_mem[2];
 	cl_mem				primitives_mem;
 	cl_mem				materials_mem;
 	cl_mem				vertices_mem;
@@ -36,6 +36,15 @@ struct s_opencl_ctx {
 	cl_mem				accel_degenerates_mem;
 	t_context			ctx[RT_WORK_OPENCL_GLOBAL_SIZE];
 	t_result			result[RT_WORK_OPENCL_GLOBAL_SIZE];
+	int					id;
+};
+
+struct s_opencl_callback_ctx {
+	t_worker			*worker;
+	struct s_opencl_ctx	*cl_ctx;
+	int					id;
+	cl_event			event;
+	size_t				size;
 };
 
 const static char *g_source_files[] = {
@@ -77,31 +86,99 @@ const static char *g_source_files[] = {
 };
 
 void
+	work_callback(cl_event event, cl_int event_command_status, void *user_data)
+{
+	struct s_opencl_callback_ctx	*cb_ctx;
+	t_worker						*worker;
+	struct s_opencl_ctx				*cl_ctx;
+	cl_int							status;
+
+	(void) event;
+	(void) event_command_status;
+	cb_ctx = user_data;
+	worker = cb_ctx->worker;
+	cl_ctx = cb_ctx->cl_ctx;
+	queue_send(&worker->queue, cl_ctx->result, sizeof(*cl_ctx->result) * cb_ctx->size);
+	status = clSetUserEventStatus(cb_ctx->event, CL_COMPLETE);
+	rt_assert(status == CL_SUCCESS, "clSetUserEventStatus work_callback failed");
+}
+
+void
 	*work_start(void *data)
 {
-	t_worker			*worker;
-	uint64_t			begin;
-	uint64_t			end;
-	struct s_opencl_ctx	*cl_ctx;
-	cl_int				status;
-	size_t				global_work_size[1];
-	size_t				local_work_size[1];
+	t_worker						*worker;
+	uint64_t						begin;
+	uint64_t						end;
+	struct s_opencl_ctx				*cl_ctx;
+	cl_int							status;
+	size_t							global_work_size[1];
+	size_t							local_work_size[1];
+	cl_event						kernel_event[2];
+	cl_event						read_event;
+	int								id;
+	struct s_opencl_callback_ctx	cb_ctx[2];
 
 	worker = data;
 	cl_ctx = worker->ctx;
 	global_work_size[0] = RT_WORK_OPENCL_GLOBAL_SIZE;
 	local_work_size[0] = RT_WORK_OPENCL_LOCAL_SIZE;
-	while (work_sync(worker->work, &begin, &end, RT_WORK_OPENCL_GLOBAL_SIZE))
+	cb_ctx[0].worker = worker;
+	cb_ctx[0].cl_ctx = cl_ctx;
+	cb_ctx[0].id = 0;
+	cb_ctx[1].worker = worker;
+	cb_ctx[1].cl_ctx = cl_ctx;
+	cb_ctx[1].id = 1;
+	if (work_sync(worker->work, &begin, &end, RT_WORK_OPENCL_GLOBAL_SIZE))
 	{
+		id = 0;
 		status = clSetKernelArg(cl_ctx->work_kernel, 2, sizeof(begin), &begin);
 		rt_assert(status == CL_SUCCESS, "clSetKernelArg work_kernel 2 failed");
 		status = clSetKernelArg(cl_ctx->work_kernel, 3, sizeof(end), &end);
 		rt_assert(status == CL_SUCCESS, "clSetKernelArg work_kernel 3 failed");
-		status = clEnqueueNDRangeKernel(cl_ctx->command_queue, cl_ctx->work_kernel, 1, NULL, global_work_size, local_work_size, 0, NULL, NULL);
+		status = clSetKernelArg(cl_ctx->work_kernel, 4, sizeof(cl_mem), &cl_ctx->result_mem[id]);
+		rt_assert(status == CL_SUCCESS, "clSetKernelArg work_kernel 4 failed");
+		cb_ctx[id].size = end - begin;
+		status = clEnqueueNDRangeKernel(cl_ctx->command_queue[0], cl_ctx->work_kernel, 1, NULL, global_work_size, local_work_size, 0, NULL, &kernel_event[id]);
 		rt_assert(status == CL_SUCCESS, "clEnqueueNDRangeKernel work_kernel failed");
-		status = clEnqueueReadBuffer(cl_ctx->command_queue, cl_ctx->result_mem, CL_TRUE, 0, sizeof(*cl_ctx->result) * RT_WORK_OPENCL_GLOBAL_SIZE, cl_ctx->result, 0, NULL, NULL);
-		rt_assert(status == CL_SUCCESS, "clEnqueueReadBuffer failed");
-		queue_send(&worker->queue, cl_ctx->result, sizeof(*cl_ctx->result) * (end - begin));
+		status = clEnqueueReadBuffer(cl_ctx->command_queue[1], cl_ctx->result_mem[id], CL_FALSE, 0, sizeof(*cl_ctx->result) * RT_WORK_OPENCL_GLOBAL_SIZE, cl_ctx->result, 1, &kernel_event[id], &read_event);
+		rt_assert(status == CL_SUCCESS, "clEnqueueReadBuffer work_kernel failed");
+		cb_ctx[id].event = clCreateUserEvent(cl_ctx->context, &status);
+		rt_assert(status == CL_SUCCESS, "clCreateUserEvent work_kernel failed");
+		status = clSetEventCallback(read_event, CL_COMPLETE, work_callback, &cb_ctx[id]);
+		rt_assert(status == CL_SUCCESS, "clSetEventCallback work_kernel failed");
+		status = clReleaseEvent(read_event);
+		rt_assert(status == CL_SUCCESS, "clReleaseEvent work_kernel failed");
+		while (work_sync(worker->work, &begin, &end, RT_WORK_OPENCL_GLOBAL_SIZE))
+		{
+			id = 1 - id;
+			status = clSetKernelArg(cl_ctx->work_kernel, 2, sizeof(begin), &begin);
+			rt_assert(status == CL_SUCCESS, "clSetKernelArg work_kernel 2 failed");
+			status = clSetKernelArg(cl_ctx->work_kernel, 3, sizeof(end), &end);
+			rt_assert(status == CL_SUCCESS, "clSetKernelArg work_kernel 3 failed");
+			status = clSetKernelArg(cl_ctx->work_kernel, 4, sizeof(cl_mem), &cl_ctx->result_mem[id]);
+			rt_assert(status == CL_SUCCESS, "clSetKernelArg work_kernel 4 failed");
+			cb_ctx[id].size = end - begin;
+			status = clEnqueueNDRangeKernel(cl_ctx->command_queue[0], cl_ctx->work_kernel, 1, NULL, global_work_size, local_work_size, 1, &kernel_event[1 - id], &kernel_event[id]);
+			rt_assert(status == CL_SUCCESS, "clEnqueueNDRangeKernel work_kernel failed");
+			status = clReleaseEvent(kernel_event[1 - id]);
+			rt_assert(status == CL_SUCCESS, "clReleaseEvent work_kernel failed");
+			status = clWaitForEvents(1, &cb_ctx[1 - id].event);
+			rt_assert(status == CL_SUCCESS, "clWaitForEvents work_kernel failed");
+			status = clReleaseEvent(cb_ctx[1 - id].event);
+			rt_assert(status == CL_SUCCESS, "clReleaseEvent work_kernel failed");
+			status = clEnqueueReadBuffer(cl_ctx->command_queue[1], cl_ctx->result_mem[id], CL_FALSE, 0, sizeof(*cl_ctx->result) * RT_WORK_OPENCL_GLOBAL_SIZE, cl_ctx->result, 1, &kernel_event[id], &read_event);
+			rt_assert(status == CL_SUCCESS, "clEnqueueReadBuffer work_kernel failed");
+			cb_ctx[id].event = clCreateUserEvent(cl_ctx->context, &status);
+			rt_assert(status == CL_SUCCESS, "clCreateUserEvent work_kernel failed");
+			status = clSetEventCallback(read_event, CL_COMPLETE, work_callback, &cb_ctx[id]);
+			rt_assert(status == CL_SUCCESS, "clSetEventCallback work_kernel failed");
+			status = clReleaseEvent(read_event);
+			rt_assert(status == CL_SUCCESS, "clReleaseEvent work_kernel failed");
+		}
+		status = clReleaseEvent(kernel_event[id]);
+		rt_assert(status == CL_SUCCESS, "clReleaseEvent work_kernel failed");
+		status = clReleaseEvent(cb_ctx[id].event);
+		rt_assert(status == CL_SUCCESS, "clReleaseEvent work_kernel failed");
 	}
 	return (NULL);
 }
@@ -109,9 +186,10 @@ void
 void
 	work_set_ptr(struct s_opencl_ctx *cl_ctx, cl_mem dst, cl_int offset, cl_mem ptr)
 {
-	size_t	global_work_size[1];
-	size_t	local_work_size[1];
-	cl_int	status;
+	size_t		global_work_size[1];
+	size_t		local_work_size[1];
+	cl_int		status;
+	cl_event	event;
 
 	global_work_size[0] = 1;
 	local_work_size[0] = 1;
@@ -121,8 +199,12 @@ void
 	rt_assert(status == CL_SUCCESS, "clSetKernelArg set_ptr_kernel 1 failed");
 	status = clSetKernelArg(cl_ctx->set_ptr_kernel, 2, sizeof(cl_mem), &ptr);
 	rt_assert(status == CL_SUCCESS, "clSetKernelArg set_ptr_kernel 2 failed");
-	status = clEnqueueNDRangeKernel(cl_ctx->command_queue, cl_ctx->set_ptr_kernel, 1, NULL, global_work_size, local_work_size, 0, NULL, NULL);
+	status = clEnqueueNDRangeKernel(cl_ctx->command_queue[0], cl_ctx->set_ptr_kernel, 1, NULL, global_work_size, local_work_size, 0, NULL, &event);
 	rt_assert(status == CL_SUCCESS, "clEnqueueNDRangeKernel set_ptr_kernel failed");
+	status = clWaitForEvents(1, &event);
+	rt_assert(status == CL_SUCCESS, "clWaitForEvents set_ptr_kernel failed");
+	status = clReleaseEvent(event);
+	rt_assert(status == CL_SUCCESS, "clReleaseEvent set_ptr_kernel failed");
 }
 
 cl_mem
@@ -147,8 +229,10 @@ void
 	rt_assert(status == CL_SUCCESS, "clCreateBuffer world failed");
 	cl_ctx->ctx_mem = clCreateBuffer(cl_ctx->context, CL_MEM_COPY_HOST_PTR, sizeof(*cl_ctx->ctx) * RT_WORK_OPENCL_GLOBAL_SIZE, cl_ctx->ctx, &status);
 	rt_assert(status == CL_SUCCESS, "clCreateBuffer ctx failed");
-	cl_ctx->result_mem = clCreateBuffer(cl_ctx->context, CL_MEM_WRITE_ONLY, sizeof(*cl_ctx->result) * RT_WORK_OPENCL_GLOBAL_SIZE, NULL, &status);
-	rt_assert(status == CL_SUCCESS, "clCreateBuffer result failed");
+	cl_ctx->result_mem[0] = clCreateBuffer(cl_ctx->context, CL_MEM_WRITE_ONLY, sizeof(*cl_ctx->result) * RT_WORK_OPENCL_GLOBAL_SIZE, NULL, &status);
+	rt_assert(status == CL_SUCCESS, "clCreateBuffer result[0] failed");
+	cl_ctx->result_mem[1] = clCreateBuffer(cl_ctx->context, CL_MEM_WRITE_ONLY, sizeof(*cl_ctx->result) * RT_WORK_OPENCL_GLOBAL_SIZE, NULL, &status);
+	rt_assert(status == CL_SUCCESS, "clCreateBuffer result[1] failed");
 	cl_ctx->primitives_mem = work_copy_array(cl_ctx, work->state->world->primitives_size, work->state->world->primitives);
 	cl_ctx->vertices_mem = work_copy_array(cl_ctx, work->state->world->vertices_size, work->state->world->vertices);
 	cl_ctx->materials_mem = work_copy_array(cl_ctx, work->state->world->materials_size, work->state->world->materials);
@@ -204,7 +288,8 @@ void
 	work_setup(t_work *work, cl_platform_id platform, cl_device_id device)
 {
 	struct s_opencl_ctx		*cl_ctx;
-	cl_context_properties	props[3];
+	cl_context_properties	context_props[3];
+	cl_queue_properties		queue_props[3];
 	cl_int					status;
 	size_t					i;
 
@@ -215,12 +300,17 @@ void
 		ctx_init(&cl_ctx->ctx[i]);
 		i += 1;
 	}
-	props[0] = CL_CONTEXT_PLATFORM;
-	props[1] = (cl_context_properties) platform;
-	props[2] = 0;
-	cl_ctx->context = clCreateContextFromType(props, CL_DEVICE_TYPE_DEFAULT, NULL, NULL, &status);
+	queue_props[0] = CL_QUEUE_PROPERTIES;
+	queue_props[1] = CL_QUEUE_OUT_OF_ORDER_EXEC_MODE_ENABLE;
+	queue_props[2] = 0;
+	context_props[0] = CL_CONTEXT_PLATFORM;
+	context_props[1] = (cl_context_properties) platform;
+	context_props[2] = 0;
+	cl_ctx->context = clCreateContextFromType(context_props, CL_DEVICE_TYPE_DEFAULT, NULL, NULL, &status);
 	rt_assert(status == CL_SUCCESS, "clCreateContextFromType failed");
-	cl_ctx->command_queue = clCreateCommandQueueWithProperties(cl_ctx->context, device, 0, &status);
+	cl_ctx->command_queue[0] = clCreateCommandQueueWithProperties(cl_ctx->context, device, queue_props, &status);
+	rt_assert(status == CL_SUCCESS, "clCreateCommandQueue failed");
+	cl_ctx->command_queue[1] = clCreateCommandQueueWithProperties(cl_ctx->context, device, queue_props, &status);
 	rt_assert(status == CL_SUCCESS, "clCreateCommandQueue failed");
 	work_create_program(cl_ctx, device);
 	cl_ctx->work_kernel = clCreateKernel(cl_ctx->program, "work_kernel", &status);
@@ -232,8 +322,6 @@ void
 	rt_assert(status == CL_SUCCESS, "clSetKernelArg work_kernel 0 failed");
 	status = clSetKernelArg(cl_ctx->work_kernel, 1, sizeof(cl_mem), &cl_ctx->ctx_mem);
 	rt_assert(status == CL_SUCCESS, "clSetKernelArg work_kernel 1 failed");
-	status = clSetKernelArg(cl_ctx->work_kernel, 4, sizeof(cl_mem), &cl_ctx->result_mem);
-	rt_assert(status == CL_SUCCESS, "clSetKernelArg work_kernel 4 failed");
 	work_add(work, work_start, cl_ctx);
 }
 
@@ -249,7 +337,6 @@ void
 	rt_assert(status == CL_SUCCESS, "clGetPlatformIDs failed");
 	status = clGetDeviceIDs(platform, CL_DEVICE_TYPE_DEFAULT, 1, &device, NULL);
 	rt_assert(status == CL_SUCCESS, "clGetDeviceIDs failed");
-	work_setup(work, platform, device);
 	work_setup(work, platform, device);
 }
 
