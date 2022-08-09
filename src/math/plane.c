704530ff2f11457c72255a1120e713015635bae7
Chen Steenvoorden <csteenvo@student.codam.nl>
2022-08-09 11:13:10 +0200

    add uv,dpdu and dpdv to plane
    Former-commit-id: f00be59fc23d10485dee852dd374e6de62dc755b [formerly 31cb24cd9017409e54aefca5081c518ee22177d3]
    Former-commit-id: ba43ef3f99e5084eae4846d967828960dc7a22c5

--- src/math/plane.c
+++ src/math/plane.c
@@ -10,6 +10,28 @@ t_plane
 	return (result);
 }
 
+static t_vec
+	plane_hit_get_tanget(t_plane plane, t_vec point)
+{
+	t_vec	op;
+
+	op = vec_sub(plane.pos, point);
+	if (rt_abs(z(plane.normal)) >= 0.999)
+		return (vec3(z(plane.normal), -x(plane.normal), 0.0));
+	return (vec3(y(plane.normal), -x(plane.normal), 0.0));
+	return (vec_norm(vec_cross(vec_norm(op), plane.normal)));
+}
+
+static t_vec2
+	plane_get_uv(t_plane plane, t_vec point, t_vec up, t_vec right)
+{
+	t_vec	op;
+
+	op = vec_sub(plane.pos, point);
+	return (vec2(vec_dot(op, up), vec_dot(op, right)));
+	return (vec2(x(op), y(op)));
+}
+
 int
 	ray_plane_intersect(t_ray ray, t_plane plane, FLOAT min, t_hit *hit)
 {
@@ -25,5 +47,8 @@ int
 	hit->t = t;
 	hit->pos = ray_at(ray, t);
 	hit->normal = plane.normal;
+	hit->dpdu = plane_hit_get_tanget(plane, hit->pos);
+	hit->dpdv = vec_norm(vec_cross(plane.normal, hit->dpdu));
+	hit->uv = plane_get_uv(plane, hit->pos, hit->dpdu, hit->dpdv);
 	return (t >= min);
 }
