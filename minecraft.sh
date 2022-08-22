53736001df8bf4c48d62d0f8223f10960a82191f
Chen Steenvoorden <csteenvo@student.codam.nl>
2022-08-22 12:04:43 +0200

    put mc files in dir
    Former-commit-id: 39f32ac853d079ae204e714cc0dfa9fb59b006ca

--- minecraft.sh
+++ minecraft.sh
@@ -1,5 +1,6 @@
+mkdir -p mc
 for i in $(seq ${2:-0} ${1:-1} 359); do
 	echo \# FRAME $i
-	name=$(printf mc%03d.ppm $i)
+	name=$(printf mc/mc%03d.ppm $i)
 	./miniRT scenes/minecraft.rt -w 960 -h 540 -k $i -r cpu -r gpu -o $name -s 500
 done
