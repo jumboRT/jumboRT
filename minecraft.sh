42ef735cc71288988906b974e8643713b9033480
Chen Steenvoorden <csteenvo@student.codam.nl>
2022-08-29 16:35:28 +0200

    change minecraft.sh
    Former-commit-id: 5373ec8324e8ffd8554a39c25d499b12a5cd6975 [formerly 50446ec9ac43b471f105c39115e1e448bdd58f5d]
    Former-commit-id: 204c90dc02e7f3f41e3b685366321f5352113b35

--- minecraft.sh
+++ minecraft.sh
@@ -2,5 +2,5 @@ mkdir -p mc
 for i in $(seq ${2:-0} ${1:-1} 3599); do
 	echo \# FRAME $i
 	name=$(printf mc/mc%04d.ppm $i)
-	./miniRT scenes/minecraft.rt -k $i -r net -o $name -s 5000 -j 32
+	./miniRT scenes/minecraft.rt -k $i -r net -o $name -s 5000
 done
