4cb8855e1980330d3a47f14bcec97a4aaa0ddf3f
Hyxogen <8938732+Hyxogen@users.noreply.github.com>
2022-09-14 14:41:51 +0200

    use better distribution for random vectors on unit sphere

--- include/cl.h
+++ include/cl.h
@@ -30,5 +30,7 @@ float	rt_random_float_range(GLOBAL t_seed *seed, float min, float max);
 t_vec	rt_random_in_sphere(GLOBAL t_seed *seed);
 t_vec	rt_random_on_hemi(GLOBAL t_seed *seed, t_vec normal);
 t_vec	rt_random_in_disk(GLOBAL t_seed *seed, t_vec right, t_vec up, float radius);
+float	rt_random_gauss(GLOBAL t_seed *seed);
+t_vec	rt_random_unit_sphere(GLOBAL t_seed *seed);
 
 #endif
