82cca04eedc0808c1227108d2d3994034ac4b705
Chen Steenvoorden <csteenvo@student.codam.nl>
2022-10-07 13:57:13 +0200

    point lights

--- src/parser/light.c
+++ src/parser/light.c
@@ -7,12 +7,11 @@
 void
 	rt_exec_light(t_world *world, t_parse_ctx *ctx)
 {
-	t_shape_sphere	sphere;
+	t_shape_point	sphere;
 	t_material		material;
 
-	sphere.base.data = RT_SHAPE_SPHERE;
+	sphere.base.data = RT_SHAPE_POINT;
 	sphere.pos = rt_vec(ctx);
-	sphere.radius = 1.0;
 	material_init(&material, world);
 	material.brightness = rt_float_range(ctx, 0.0, 1.0) * 256;
 	material.emission = rt_filter(world, ctx);
