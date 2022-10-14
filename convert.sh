73b85cac4311b8b439dc113fa55ab5221992e4db
Chen Steenvoorden <csteenvo@student.codam.nl>
2022-10-14 11:01:52 +0200

    delete unused scripts

--- convert.sh
+++
@@ -1,1 +0,0 @@
-magick $1 -colorspace sRGB -depth 32 -alpha set "$(basename $1).bmp"
--- minecraft.py
+++
@@ -1,9 +0,0 @@
-import math
-
-z = -1 / 3
-a = math.sqrt(1 - z * z)
-
-for i in range(3600):
-    x = math.cos(i / 1800 * math.pi) * a
-    y = math.sin(i / 1800 * math.pi) * a
-    print(f"$ {i} C 61,-69,77 {x:.05f},{y:.05f},{z:.05f} 90")
