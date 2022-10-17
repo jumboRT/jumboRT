2fdc54afede6dd4b900b5f86b0b1f5152911c271
Hyxogen <8938732+Hyxogen@users.noreply.github.com>
2022-10-17 11:58:02 +0200

    sanitize input for boloids

--- src/parser/boloid.c
+++ src/parser/boloid.c
@@ -11,7 +11,11 @@ void
 	paraboloid.base.data = RT_SHAPE_PARABOLOID;
 	paraboloid.paraboloid.pos = rt_vec(ctx);
 	paraboloid.paraboloid.a = rt_float(ctx);
+	if (paraboloid.paraboloid.a == 0)
+		rt_parse_error(ctx, "variable a of a paraboloid may not be zero");
 	paraboloid.paraboloid.b = rt_float(ctx);
+	if (paraboloid.paraboloid.b == 0)
+		rt_parse_error(ctx, "variable b of a paraboloid may not be zero");
 	rt_material(ctx, world, &paraboloid.base);
 	world_add_primitive(world, &paraboloid, sizeof(paraboloid));
 }
@@ -24,8 +28,14 @@ void
 	hyperboloid.base.data = RT_SHAPE_HYPERBOLOID;
 	hyperboloid.hyperboloid.pos = rt_vec(ctx);
 	hyperboloid.hyperboloid.a = rt_float(ctx);
+	if (hyperboloid.hyperboloid.a == 0)
+		rt_parse_error(ctx, "variable a of a hyperboloid may not be zero");
 	hyperboloid.hyperboloid.b = rt_float(ctx);
+	if (hyperboloid.hyperboloid.b == 0)
+		rt_parse_error(ctx, "variable b of a hyperboloid may not be zero");
 	hyperboloid.hyperboloid.c = rt_float(ctx);
+	if (hyperboloid.hyperboloid.c == 0)
+		rt_parse_error(ctx, "variable c of a hyperboloid may not be zero");
 	rt_material(ctx, world, &hyperboloid.base);
 	world_add_primitive(world, &hyperboloid, sizeof(hyperboloid));
 }
